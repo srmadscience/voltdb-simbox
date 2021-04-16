894a6bfdd2ca2c7561ba7feb4b986632b00eaeca
David Rolfe <drolfe@voltdb.com>
2021-04-16 14:36:14 +0100

    Added DELETE FROM simbox_stats;

--- ddl/create_db.sql
+++ ddl/create_db.sql
@@ -248,6 +248,19 @@ upsert into simbox_parameters
 VALUES
 ('SIMBOX_CALLS_ITSELF',0);
 
+UPSERT INTO simbox_stats VALUES ('simbox_stats_simboxstatus_not_suspected',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_simboxstatus_some_incoming_calls_from_known_bad_numbers',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_simboxstatus_suspicious_device_has_no_incoming_calls',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_simboxstatus_suspiciously_moving_device',0);
+
+UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_some_incoming_calls_from_known_bad_numbers',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_suspicious_device_has_no_incoming_calls',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_suspiciously_moving_device',0);
+UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_all_incoming_calls_from_known_bad_numbers',0);
+ UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_total_incoming_outgoing_ratio_bad',0);
+ UPSERT INTO simbox_stats VALUES ('simbox_stats_suspicious_because_topn_incoming_outgoing_ratio_bad',0);
+                                                                                                          
+
 
 
 
